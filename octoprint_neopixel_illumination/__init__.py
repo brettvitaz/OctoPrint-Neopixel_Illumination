c03a84849d81ab8410a3e553f94366c79f2d45b9
brettvitaz <brett@vitaz.net>
2022-01-15 13:55:12 -0800

    Add neopixel library, some test code

--- octoprint_neopixel_illumination/__init__.py
+++ octoprint_neopixel_illumination/__init__.py
@@ -11,10 +11,12 @@ from __future__ import absolute_import
 
 import octoprint.plugin
 
-class Neopixel_illuminationPlugin(octoprint.plugin.SettingsPlugin,
-    octoprint.plugin.AssetPlugin,
-    octoprint.plugin.TemplatePlugin
-):
+
+class NeopixelIlluminationPlugin(octoprint.plugin.SettingsPlugin,
+                                 octoprint.plugin.AssetPlugin,
+                                 octoprint.plugin.TemplatePlugin,
+                                 octoprint.plugin.StartupPlugin
+                                 ):
 
     ##~~ SettingsPlugin mixin
 
@@ -56,6 +58,9 @@ class Neopixel_illuminationPlugin(octoprint.plugin.SettingsPlugin,
             }
         }
 
+    def on_after_startup(self):
+        self._logger.info("Hello Neopixel")
+
 
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
@@ -65,13 +70,12 @@ __plugin_name__ = "Neopixel_illumination Plugin"
 # Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
 # Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
 # compatibility flags according to what Python versions your plugin supports!
-#__plugin_pythoncompat__ = ">=2.7,<3" # only python 2
-#__plugin_pythoncompat__ = ">=3,<4" # only python 3
-#__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
+__plugin_pythoncompat__ = ">=3,<4"  # only python 3
+
 
 def __plugin_load__():
     global __plugin_implementation__
-    __plugin_implementation__ = Neopixel_illuminationPlugin()
+    __plugin_implementation__ = NeopixelIlluminationPlugin()
 
     global __plugin_hooks__
     __plugin_hooks__ = {
--- setup.py
+++ setup.py
@@ -33,7 +33,9 @@ plugin_url = "https://github.com/brettvitaz/OctoPrint-Neopixel_Illumination"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
+plugin_requires = [
+    "adafruit-circuitpython-neopixel"
+]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
